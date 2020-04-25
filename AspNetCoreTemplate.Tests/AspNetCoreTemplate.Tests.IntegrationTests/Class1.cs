84bbbdf9a2464985094e3247286a76b9562d7084
Svetloslav15 <sn_novoselski@abv.bg>
2020-04-25 23:40:08 +0300

    add tests assemblies

---
+++ AspNetCoreTemplate.Tests/AspNetCoreTemplate.Tests.IntegrationTests/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AspNetCoreTemplate.Tests.IntegrationTests
+{
+    public class Class1
+    {
+    }
+}
---
+++ AspNetCoreTemplate.Tests/AspNetCoreTemplate.Tests.UnitTests/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AspNetCoreTemplate.Tests.UnitTests
+{
+    public class Class1
+    {
+    }
+}
