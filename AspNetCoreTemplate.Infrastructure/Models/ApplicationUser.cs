a76c5bb216b2be72af6a983b3b1adccd80c7ad16
Svetloslav15 <sn_novoselski@abv.bg>
2020-04-26 23:50:17 +0300

    add new fields to application users

--- AspNetCoreTemplate.Infrastructure/Models/ApplicationUser.cs
+++ AspNetCoreTemplate.Infrastructure/Models/ApplicationUser.cs
@@ -1,8 +1,17 @@
 ﻿namespace AspNetCoreTemplate.Infrastructure.Models
 {
+    using AspNetCoreTemplate.Infrastructure.Models.Common;
     using Microsoft.AspNetCore.Identity;
+    using System;
 
-    public class ApplicationUser : IdentityUser
+    public class ApplicationUser : IdentityUser, IAuditInfo, IDeletableEntity
     {
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
     }
 }
